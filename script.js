46b3de6f30a684db34da8afca0cd1db145742be6
DeepLakePR <guilhermefortunato671@gmail.com>
2025-02-20 00:02:26 -0300

    ROBSON?

---
+++ script.js
@@ -0,0 +1,67 @@
+$(() => {
+
+    var active = false;
+
+    const yes = $('#yes');
+    const no = $('#no');
+
+    no.click(() => {
+
+        let vTop = Math.floor(Math.random() * ($(window).height() - 200)) + 1;
+        let vLeft = Math.floor(Math.random() * ($(window).width() - 400)) + 1
+
+        if (!active) {
+            active = true;
+
+            no.detach().appendTo('body');
+
+            no.css({
+                'position': 'absolute',
+                'top': vTop + 'px',
+                'left': vLeft + 'px'
+            })
+            $('i').css('left', '48%');
+
+        }
+
+    });
+
+    no.on('mouseover', () => {
+
+        let vTop = Math.floor(Math.random() * ($(window).height() - 200)) + 1;
+        let vLeft = Math.floor(Math.random() * ($(window).width() - 400)) + 1
+
+        if (active) {
+            no.css({
+                'position': 'absolute',
+                'top': vTop + 'px',
+                'left': vLeft + 'px'
+            })
+        }
+    });
+
+    yes.click(()=>{
+
+        yes.detach().appendTo('body');
+
+        yes.css({
+            'position': 'absolute',
+            'background': 'rgba(0, 210, 0, 1)',
+        })
+
+        yes.animate({
+            'width': '100%',
+            'height': '100%',
+            'font-size': '100px'
+        }, 5000, ()=>{
+
+            window.open('https://www.youtube.com/watch?v=HR4ZxjGQGYY');
+            window.open('https://vm.tiktok.com/ZMkvkbLRL/');
+            window.open('https://www.youtube.com/watch?v=FJZIl0JPmgs');
+            window.open(' https://www.youtube.com/watch?v=o9zEziMKAkQ');
+        
+        });
+
+    });
+
+});
